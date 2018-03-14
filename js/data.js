b4d73e22124e8aaae71620d9037c36dd4b54e44d
arintaosorio <arintaosorio@gmail.com>
2018-03-13 20:51:06 -0600

    funcion index

---
+++ js/data.js
@@ -0,0 +1,42 @@
+const products = [
+    {
+        img:"./assets/imagesProductosData/tinte.jpg",
+        nombre: 'Tinte Loreal',
+        precio: 65,
+    },
+    {
+        img:"./assets/imagesProductosData/cremaExfoliante.jpg",
+        nombre:'Crema Exfoliante',
+        precio:80,
+    },
+    {
+        img:"./assets/imagesProductosData/shampoo.jpg",
+        nombre:'Shampoo',
+        precio:75,
+    },
+    {
+        img:"./assets/imagesProductosData/listerine.jpg",
+        nombre:'Listerine',
+        precio:65,
+    },
+    {
+        img:"./assets/imagesProductosData/detergenteIntimo.jpg",
+        nombre:'Detergente Intimo',
+        precio:125,
+    },
+    {
+        img:"./assets/imagesProductosData/labiales.jpg",
+        nombre:'Labiales',
+        precio:95,
+    },
+    {
+        img:"./assets/imagesProductosData/sombras.jpg",
+        nombre:'Sombras',
+        precio:69,
+    },
+    {
+        img:"./assets/imagesProductosData/corrector.jpg",
+        nombre:'Corrector',
+        precio:90,
+    }
+]
--- js/drawInHtml.js
+++ js/drawInHtml.js
@@ -1,5 +1,3 @@
-
-
 let contentProducts = document.getElementById('container-products');//Elemento al que se le pintará la cuenta los productos agregados.
 let templateProduct = ``;
 const drawProducts = (id, image, name, price, rating, sold, originalPrice, currency, chainText) => {
@@ -35,3 +33,29 @@ const drawProducts = (id, image, name, price, rating, sold, originalPrice, curre
 
 
 //drawProducts();
+//
+const drawProductsHome = (imagen, nombre, precio) => {
+//   <h3>${product.title}</h3>
+let contentProducts = document.getElementById('container-products');//Elemento al que se le pintará la cuenta los productos agregados.
+let templateProduct = '';
+for (var i = 0; i < 10; i++) {
+      templateProduct += `  <div class="col-md-3 sale-product">
+        <div class="owl-item active" style="width: 188px; margin-right: 0px;"><div>
+        <div class="product-item">
+        <div class="pi-img-wrapper">
+        <img src=${imagen} class="img-responsive" alt=${nombre}>
+        <div>
+        <a href=${imagen} class="btn btn-default fancybox-button">Zoom</a>
+        <a href="#product-pop-up" class="btn btn-default fancybox-fast-view">Ver</a>
+        </div>
+        </div>
+        <h3><a href="#">${nombre}</a></h3>
+        <div class="pi-price">${precio}</div>
+        <a  href="javascript:;" class="btn btn-default add2cart btnAddRemove">Añadir a carrito</a>
+        <div class="sticker sticker-sale"></div>
+        </div>
+        </div></div></div>
+        `;
+  }}
+ console.log(contentProducts    );
+   contentProducts.innerHTML = templateProduct;
